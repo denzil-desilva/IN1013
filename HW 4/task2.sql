34d4b01341034fa59d8471c810f7be61a6c90f11
Denzil08 <Denzil.De-silva@city.ac.uk>
2024-10-22 10:46:38 +0100

    Finished

--- HW 4/task2.sql
+++ HW 4/task2.sql
@@ -1,27 +1,28 @@
 USE Pets;
 
 INSERT INTO petEvent VALUES
-    ("Fluffy", "2020-10-15", "vet", "antibiotics")
+    ("Fluffy", "2020-10-15", "vet", "antibiotics");
 
 INSERT INTO petPet VALUES
-    ("Hammy", "Diane", "hamster", "M", "2010-10-30", null)
+    ("Hammy", "Diane", "hamster", "M", "2010-10-30", NULL);
 
 INSERT INTO petEvent VALUES
-    ("Hammy", "2020-10-15", "vet", "antibiotics")
+    ("Hammy", "2020-10-15", "vet", "antibiotics");
 
-UPDATE TABLE petEvent 
+UPDATE petEvent 
 SET remark = "5 kittens, 3 female, 2 male" 
 WHERE petName = "Fluffy" AND eventdate = "1995-05-15";
 
-UPDATE TABLE petEvent 
+UPDATE petEvent 
 SET petName = "Claws" 
 WHERE remark = "broken rib" AND eventdate = "1997-08-03";
 
-UPDATE TABLE petPet 
+UPDATE petPet 
 SET death = "2020-09-01" 
 WHERE petName = "Puffball";
 
-DELETE FROM petPet INNER JOIN petEvent ON petPet.petName = petEvent.petName 
-WHERE owner = "Harold";
+DELETE petEvent.* FROM petEvent INNER JOIN petPet ON petPet.petName = petEvent.petName WHERE petPet.owner = "Harold";
+DELETE FROM petPet WHERE owner = "Harold";
 
 SELECT * FROM petPet;
+SELECT * FROM petEvent;
